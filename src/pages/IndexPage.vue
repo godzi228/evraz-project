8657e8baf025b407288143d4979b464cf06f5875
godzi228 <zimarevelisej@gmail.com>
2025-04-10 20:01:52 +0500

    магазин мета

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-card
+    class="my-card text-white"
+    style="background: #80daeb"
+  >
+    <q-card-section>
+      <div class="text-h6">Вход</div>
+    </q-card-section>
+
+    <q-input class="phNum" standout="bg-teal text-white" v-model="text" mask="+7 (###) ###-##-##" label="Номер телефона" />
+
+  </q-card>
 </template>
 
 <script setup>
-//
+
 </script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 600px
+  height: 400px
+  margin: 200px auto
+
+.text-h6
+  color: #000000
+  margin-left: 228px
+  margin-top: 39px
+  font-size: 43px
+
+.phNum
+  width: 67ch
+  margin: 30px auto
+  background: #80daeb
+</style>
