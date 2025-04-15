32bdb8c902c95dac61949b35f2f564172dbe68d9
ToKeRNiK <nevobuxnt@gmail.com>
2025-04-15 19:14:45 +0500

    Ты кто

--- src/pages/inoformation.vue
+++ src/pages/inoformation.vue
@@ -5,7 +5,7 @@
 <template>
   <q-card class="my-card">
     <q-card-section>
-      <q-img src="https://avatars.mds.yandex.net/i?id=2a000001962a894f0d0bfcd20e27ac68496a-1614014-fast-images&n=13"></q-img>
+      <q-img class="image" src="https://avatars.mds.yandex.net/i?id=2a000001962a894f0d0bfcd20e27ac68496a-1614014-fast-images&n=13"></q-img>
     </q-card-section>
   </q-card>
 </template>
@@ -15,4 +15,7 @@
   height: 100px
   width: 88%
   margin-left: 110px
+.image
+  width: 9%
+
 </style>
