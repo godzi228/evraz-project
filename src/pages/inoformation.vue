0b020bf0e35bed50947fa3f010b9d32c463e154b
ToKeRNiK <nevobuxnt@gmail.com>
2025-04-15 19:31:43 +0500

    Проверка

--- src/pages/inoformation.vue
+++ src/pages/inoformation.vue
@@ -3,19 +3,42 @@
 </script>
 
 <template>
+  <div class="container1">
   <q-card class="my-card">
     <q-card-section>
       <q-img class="image" src="https://avatars.mds.yandex.net/i?id=2a000001962a894f0d0bfcd20e27ac68496a-1614014-fast-images&n=13"></q-img>
     </q-card-section>
   </q-card>
+  </div>
+  <div class="container">
+  <q-card class="main-card">
+    <q-card-section>
+      <q-img class="image1" src="https://images-eu.ssl-images-amazon.com/images/I/4160rSZjMdL.jpg"></q-img>
+      <div class="">Футболка с хайзенбергом</div>
+    </q-card-section>
+  </q-card>
+  </div>
 </template>
 
 <style scoped lang="sass">
+.container1
+  display: flex
+  justify-content: center
 .my-card
   height: 100px
   width: 88%
-  margin-left: 110px
 .image
   width: 9%
 
+.container
+  display: flex
+  justify-content: center
+
+.main-card
+  width: 88%
+  height: 400px
+  margin-top: 100px
+
+.image1
+  width: 18%
 </style>
